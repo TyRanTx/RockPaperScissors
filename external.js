a638b4b245a1cd12fb3e798fd7b4d403a49095c0
Samir Qureshi <samir.h.qureshi@gmail.com>
2022-12-22 22:25:47 +0500

    Add a function playRound()

--- external.js
+++ external.js
@@ -1,13 +1,36 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let randomValue = Math.floor(Math.random() * 3);
-    switch(randomValue){
+    switch (randomValue) {
         case 0:
-            return 'Rock';
+            return 'rock';
         case 1:
-            return 'Paper';
+            return 'paper';
         case 2:
-            return 'Scissor';
+            return 'scissor';
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+
+    playerSelectionLower = playerSelection.toLowerCase();
+
+    if (playerSelectionLower === computerSelection) {
+        return "It's a draw!" + ' Computer Selection was: ' + computerSelection;
+    }
+    else if(playerSelectionLower != 'rock' && playerSelectionLower !='paper' && playerSelectionLower !='scissor'){
+        return 'Wrong value';
+    }
+    else if (playerSelectionLower === 'rock' && computerSelection === 'scissor') {
+        return 'You won' + ' Computer Selection was: ' + computerSelection;
+    }
+    else if (playerSelectionLower === 'scissor' && computerSelection === 'paper') {
+        return 'You won' + ' Computer Selection was: ' + computerSelection;
+    }
+    else if (playerSelectionLower === 'paper' && computerSelection === 'rock') {
+        return 'You won' + ' Computer Selection was: ' + computerSelection;
+    }
+    else
+        return 'You lost' + ' Computer Selection was: ' + computerSelection;
+}
+
+console.log(playRound('ScissoR', getComputerChoice()));
