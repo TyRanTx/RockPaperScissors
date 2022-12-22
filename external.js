532673cea21e2256b7d0c37f3c65f3de2219453b
Samir Qureshi <samir.h.qureshi@gmail.com>
2022-12-22 21:17:23 +0500

    Add a function getComputerChoice()

---
+++ external.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+    let randomValue = Math.floor(Math.random() * 3);
+    switch(randomValue){
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissor';
+    }
+}
+
+console.log(getComputerChoice());
